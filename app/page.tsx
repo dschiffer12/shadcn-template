2887a395d5415da7e12f3b245aeb606ebe9d18da
Dustin Schiffer <dschiffer12@gmail.com>
2023-04-20 22:08:46 +0000

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,68 @@
 "use client"
 
 import { useState } from 'react';
-import Papa from 'papaparse';
+import Papa, { ParseMeta, parse } from 'papaparse';
+import ReactPivottableUI from 'react-pivottable';
+import OpenAI from 'openai-api';
+import {
+  Box,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Spinner,
+} from '@chakra-ui/react';
 import PivotTableUI from 'react-pivottable/PivotTableUI';
-import 'react-pivottable/pivottable.css';
-import  OpenAI  from 'openai-api';
-import { error } from 'console';
-import { FormErrorMessage } from '@chakra-ui/react';
+import TableRenderers from 'react-pivottable/TableRenderers';
+
+interface CSVData {
+  data: CSVRow[];
+  meta: {
+    fields: string[];
+  };
+  onchange: (data: CSVData) => void;
+
+}
+
+interface CSVRow {
+  [key: string]: string;
+}
+
+function parseCSV<T extends CSVRow>(csv: string): T[] {
+  const lines = csv.split('');
+  const [header, ...rows] = lines;
+  const keys = header.split(',');
+  const data: T[] = [];
+
+  for (const row of rows) {
+    const values = row.split(',');
+    const item: any = {};
+
+    for (let i = 0; i < values.length; i++) {
+      item[keys[i]] = values[i];
+    }
+
+    data.push(item);
+  }
+
+  return data;
+}
+
+const csv = 'csvData'
+const data = parseCSV(csv);
+
+console.log(data);
 
 const FileUploader = () => {
   const [file, setFile] = useState<File>();
   const [data, setData] = useState<string[][]>([]);
+  const [csvData, setCsvData] = useState<CSVData>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [response, setResponse] = useState<string>("");
+  const [response, setResponse] = useState<string>();
+  const [error, setError] = useState<string>();
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return;
 
@@ -22,18 +70,25 @@ const FileUploader = () => {
     setError('');
 
     const reader = new FileReader();
-    reader.onload = (e) => {
-      const fileContent = e.target?.result;
-      if (fileContent) {
-        const { data } = Papa.parse(fileContent.toString());
-        setData(data as any[][]);
-      }
-    };
-    reader.onerror = (e) => setError(e.target?.error?.message || 'Unknown error');
-    reader.readAsText(file);
-    setIsLoading(false);
+  const onFileRead = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const fileContent = event.target?.files?.[0];
+    if (fileContent) {
+      const reader = new FileReader();
+      setIsLoading(true);
+      reader.onload = (e) => {
+        const fileContent = e.target?.result;
+        if (fileContent) {
+          const { data, meta }: { data: CSVRow[], meta: ParseMeta } = parse(fileContent.toString(), {
+            header: true,
+          });
+          const csvData: CSVData = { data, meta:meta.fields, onChange: (data: CSVData) => {} };
+          setCsvData(csvData);
+        }
+        setIsLoading(false);
+      };
+      reader.readAsText(fileContent);
+    }
   };
-
   const handleAskQuestion = async (question: string) => {
     if (!OpenAI) return;
 
@@ -42,49 +97,51 @@ const FileUploader = () => {
 
     try {
       const context = data.map((row) => row.join(' ')).join(' ');
-      const response = await OpenAI.arguments(handleAskQuestion);
-      setOutput(response.data);
+      const response = await OpenAI.arguments(question, context);
+      setResponse(response.data);
     } catch (error) {
       console.error(error);
       setError('An error occurred, please try again.');
     }
     setIsLoading(false);
   };
-
+  const handleFieldChange = (field: string | undefined, value: any) => {
+    if (field && csvData) {
+      const newCsvData: CSVData = {
+        ...csvData,
+        [field]: value,
+      };
+      setCsvData(newCsvData);
+    }
+  };
   return (
-    <div>
-      <h1>Spreadsheet Interpreter</h1>
-      <input type="file" onChange={handleFileChange} />
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-
-          <p>{response}</p>
-          <p>{setOutput}</p>
-          <p>{data}</p>
-          <p>{isLoading}</p>
-          <p>Loaded {data.length} rows of data</p>
-          <OpenAI arguments={handleAskQuestion}>
-            <QuestionForm onSubmit={handleAskQuestion} />
-            {output && <PivotTableUI data={output} />}
-          </OpenAI>
-
-
-        </>
-
-
+    <Box p={5}>
+      <Heading mb={5}>Pivot Table App</Heading>
+      <FormControl mb={5}>
+        <FormLabel>Upload CSV file:</FormLabel>
+        <Input type="file" onChange={handleFileChange} />
+      </FormControl>
+      {isLoading && (
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
+          alignSelf="center"
+        />
       )}
-    </div>
+      {csvData && (
+        <PivotTableUI
+        {...csvData}
+        renderers={Object.assign({}, TableRenderers, {
+            TableWithBarchart: TableRenderers.Table,
+          })}
+
+          
+        />
+      )}
+    </Box>
   );
 };
-
-export default FileUploader;
-
-function setError(_arg0: string) {
-  throw new Error('Function not implemented.');
 }
-function setOutput(data: any) {
-  throw new Error('Function not implemented.');
-}
-
